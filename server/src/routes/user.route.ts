041cf15d8ae640f896193b2ad30a3bc43924421c
Chandan R <Chandn-R@users.noreply.github.com>
2025-07-18 23:04:02 +0530

    add search user route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoutes.js";
-import { followUser, getUser, unfollowUser, updateProfile } from "../controllers/user.controller.js";
+import {
+    followUser,
+    getAllUsers,
+    getUser,
+    unfollowUser,
+    updateProfile,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.patch("/:id/follow", protectRoute, followUser);
 router.patch("/:id/unfollow", protectRoute, unfollowUser);
-router.patch("/profile", protectRoute, upload.single("profilePicture"), updateProfile);
+router.patch(
+    "/profile",
+    protectRoute,
+    upload.single("profilePicture"),
+    updateProfile
+);
 router.get("/:id", protectRoute, getUser);
-
+router.get("/search", protectRoute, getAllUsers);
 
 export default router;
